eb6a9129ddc0c9a2f089d3a424e9c46da49908fb
Tibira Botto <tibira.botto@gmail.com>
2024-03-28 22:41:47 -0400

    Create Main then gets movie from API

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const KEY = process.env.REACT_APP_IMDB_API_KEY;
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${KEY}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${KEY}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${KEY}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${KEY}&language=en-US&page=1`,
+};
+
+export default requests;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Main from "../components/Main";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 };
 
 export default Home;
