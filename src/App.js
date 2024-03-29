689d10d0874cb0bf36c55761e09a9f33171d4299
Tibira Botto <tibira.botto@gmail.com>
2024-03-29 10:26:05 -0400

    Create AuthContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
+    </AuthContextProvider>
   );
 }
 
