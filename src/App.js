73e9575e4a18a599a08b24498d54821e2007afc5
Tibira Botto <tibira.botto@gmail.com>
2024-03-29 12:27:47 -0400

    Create Signup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import Home from "./pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </AuthContextProvider>
   );
